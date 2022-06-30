11656a2f7b7e4709f9defd2a61dd2bd46a34a903
Saksham-P <84263133+Saksham-P@users.noreply.github.com>
2022-06-29 21:37:57 -0600

    parallax effect added

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,16 @@ document.addEventListener("mousemove", parallax);
 const elem = document.getElementById("mainPage");
 const elem1 = document.getElementById("left");
 
+
 function parallax(e) {
     let windowWidth = window.innerWidth/2;
     let windowHeight = window.innerHeight/2;
     let mouseX = e.clientX;
     let mouseY = e.clientY;
-    let backgroundX = `${50 - (mouseX - windowWidth) * 0.03}% `;
+    let backgroundX = `${50 - (mouseX - windowWidth) * 0.02}% `;
     let backgroundY = `${50 - (mouseY - windowHeight) * 0.05}%`;
-    let foregroundX = `${50 - (mouseX - windowWidth) * 0.01}%`;
-    let foregroundY = `${50 - (mouseY - windowHeight) * 0.03}%`;
+    let foregroundX = `${0 - (mouseX - windowWidth) * 0.01}%`;
+    let foregroundY = `${0 - (mouseY - windowHeight) * 0.03}%`;
     let x = `${backgroundX}`;
     let y = `${backgroundY}`;
     let x1 = `${foregroundX}`;
